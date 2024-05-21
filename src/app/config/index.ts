3da94353bee7ef4543614a6151e0801f9ab03f79
nhasan97 <nh6069325@gmail.com>
2024-05-21 21:00:38 +0600

    configured eslint and prettier for typescript, added custome scripts for eslint, tsc and prettier in package.json, created a basic server in server.ts and kept the app in app.ts

---
+++ src/app/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  port: process.env.PORT,
+  dbLocalUri: process.env.DB_LOCAL_URI,
+};
