5ab0ff3b9d9be2b2fe908d95ecab9e91e22c17e2
nhasan97 <nh6069325@gmail.com>
2024-05-22 09:45:13 +0600

    created type Variant, Inventory and Product in product.interface.ts, created variantSchema, inventorySchema, productSchema and model Product in product.model.ts

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import { Product, Variant, Inventory } from './product.interface';
+
+const variantSchema = new Schema<Variant[]>([
+  {
+    type: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+]);
+
+const inventorySchema = new Schema<Inventory>({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const productSchema = new Schema<Product>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: variantSchema,
+  inventory: inventorySchema,
+});
+
+const Product = model<Product>('Products', productSchema);
